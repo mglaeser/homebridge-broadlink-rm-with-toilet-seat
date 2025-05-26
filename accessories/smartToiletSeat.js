114c6d87b364985c5c38d3643f77e5ef80688c0b
Marian Gläser <mglaeser@me.com>
2025-05-27 01:15:04 +0200

    Deep Research

--- accessories/smartToiletSeat.js
+++ accessories/smartToiletSeat.js
@@ -1,173 +1,90 @@
-// accessories/smartToiletSeat.js
-
-const BroadlinkRMAccessory = require('./accessory');
+// smartToiletSeat.js
+const BroadlinkRMAccessory = require('./accessory');  // Base class from Broadlink plugin
+const { ServiceManagerTypes } = require('../helpers/serviceManagerTypes');  // Broadlink service manager factory
+const { Service, Characteristic } = require('hap-nodejs');  // Homebridge HAP classes (if needed for constants)
 
 class SmartToiletSeat extends BroadlinkRMAccessory {
-  constructor(log, config = {}) {
-    if (!config.name) config.name = 'Smart Toilet Seat';
-
-    // Konvertiere numerische Hex-Codes zu Strings
-    SmartToiletSeat.convertConfigHexCodes(config);
-
-    super(log, config);
-
-    this.checkConfig(config);
-
-    // Initialisiere den Zustand
-    this.state = {
-      powerState: false,
-      powerSaveState: false,
-      showerActive: false,
-      bidetActive: false,
-      dryActive: false,
-    };
-
-    // Zeitüberschreitungen für Wasserfunktionen
-    this.waterFunctionTimeouts = {};
+  constructor(log, config = {}, serviceManagerType) {
+    super(log, config, serviceManagerType); 
+    // You may set default config values here if needed
   }
 
-  static convertConfigHexCodes(config) {
-    if (config.data) {
-      Object.keys(config.data).forEach((key) => {
-        if (typeof config.data[key] === 'number') {
-          config.data[key] = config.data[key].toString();
-        }
-      });
-    }
-  }
-
-  checkConfig(config) {
-    const { data } = config;
-    if (!data) {
-      this.log(`${config.name} checkConfig: No data found in config`);
-      return;
-    }
-
-    const requiredCodes = [
-      'powerOn',
-      'powerOff',
-      'powerSaveOn',
-      'powerSaveOff',
-      'showerOn',
-      'showerOff',
-      'bidetOn',
-      'bidetOff',
-      'dryOn',
-      'dryOff',
-    ];
-    const missingCodes = requiredCodes.filter((code) => !data[code]);
-    if (missingCodes.length > 0) {
-      this.log(`${config.name} checkConfig: Missing hex codes: ${missingCodes.join(', ')}`);
-    } else {
-      this.log(`${config.name} checkConfig: All required hex codes found`);
-    }
-  }
-
-  configureServiceManager() {
-    const { data, name, log } = this;
-    const { Service, Characteristic } = this;
-
-    // Hauptstromversorgung (Switch)
-    const powerService = new Service.Switch(`${name} Power`, 'power');
-    powerService.getCharacteristic(Characteristic.On)
-      .onGet(() => this.state.powerState)
-      .onSet(async (value) => {
-        this.state.powerState = value;
-        const hexData = value ? data.powerOn : data.powerOff;
-        if (hexData) await this.performSend(hexData);
-      });
-    this.addService(powerService);
-
-    // Energiesparmodus (Outlet)
-    const powerSaveService = new Service.Outlet(`${name} Power Save`, 'powersave');
-    powerSaveService.getCharacteristic(Characteristic.On)
-      .onGet(() => this.state.powerSaveState)
-      .onSet(async (value) => {
-        this.state.powerSaveState = value;
-        const hexData = value ? data.powerSaveOn : data.powerSaveOff;
-        if (hexData) await this.performSend(hexData);
-      });
-    powerSaveService.getCharacteristic(Characteristic.OutletInUse)
-      .onGet(() => this.state.powerSaveState);
-    this.addService(powerSaveService);
-
-    // Dusche (Valve)
-    const showerService = new Service.Valve(`${name} Shower`, 'shower');
-    showerService.setCharacteristic(Characteristic.ValveType, Characteristic.ValveType.SHOWER);
-    showerService.getCharacteristic(Characteristic.Active)
-      .onGet(() => this.state.showerActive ? Characteristic.Active.ACTIVE : Characteristic.Active.INACTIVE)
-      .onSet(async (value) => {
-        const isActive = value === Characteristic.Active.ACTIVE;
-        await this.setWaterFunction('shower', isActive);
-      });
-    showerService.getCharacteristic(Characteristic.InUse)
-      .onGet(() => this.state.showerActive ? Characteristic.InUse.IN_USE : Characteristic.InUse.NOT_IN_USE);
-    this.addService(showerService);
-
-    // Bidet (Valve)
-    const bidetService = new Service.Valve(`${name} Bidet`, 'bidet');
-    bidetService.setCharacteristic(Characteristic.ValveType, Characteristic.ValveType.SHOWER);
-    bidetService.getCharacteristic(Characteristic.Active)
-      .onGet(() => this.state.bidetActive ? Characteristic.Active.ACTIVE : Characteristic.Active.INACTIVE)
-      .onSet(async (value) => {
-        const isActive = value === Characteristic.Active.ACTIVE;
-        await this.setWaterFunction('bidet', isActive);
-      });
-    bidetService.getCharacteristic(Characteristic.InUse)
-      .onGet(() => this.state.bidetActive ? Characteristic.InUse.IN_USE : Characteristic.InUse.NOT_IN_USE);
-    this.addService(bidetService);
-
-    // Trockner (Fan)
-    const dryService = new Service.Fan(`${name} Dry`, 'dry');
-    dryService.getCharacteristic(Characteristic.On)
-      .onGet(() => this.state.dryActive)
-      .onSet(async (value) => {
-        this.state.dryActive = value;
-        const hexData = value ? data.dryOn : data.dryOff;
-        if (hexData) await this.performSend(hexData);
-      });
-    this.addService(dryService);
-  }
-
-  async setWaterFunction(functionType, active) {
-    const { data, log, name } = this;
-    const stateKey = `${functionType}Active`;
-    const hexKey = `${functionType}${active ? 'On' : 'Off'}`;
-
-    if (data[hexKey]) {
-      await this.performSend(data[hexKey]);
-      this.state[stateKey] = active;
-
-      // Bestehenden Timeout löschen
-      if (this.waterFunctionTimeouts[functionType]) {
-        clearTimeout(this.waterFunctionTimeouts[functionType]);
-        delete this.waterFunctionTimeouts[functionType];
+  // Override the base method to set up multiple services
+  setupServiceManager() {
+    const { name, log, data, serviceManagerType } = this;
+    // Service 1: Switch for main power
+    this.serviceManagerPrimary = new ServiceManagerTypes[serviceManagerType](name, Service.Switch, log);
+    this.serviceManagerPrimary.addToggleCharacteristic({
+      name: 'powerState',
+      type: Characteristic.On,
+      getMethod: this.getCharacteristicValue,
+      setMethod: this.setCharacteristicValue,
+      bind: this,
+      props: {
+        onData: data.powerOn,   // IR code for power ON
+        offData: data.powerOff  // IR code for power OFF
       }
-
-      // Auto-Off für Wasserfunktionen (60 Sekunden)
-      if (active) {
-        log(`${name} ${functionType}: Auto-off in 60 seconds`);
-        this.waterFunctionTimeouts[functionType] = setTimeout(async () => {
-          this.state[stateKey] = false;
-          await this.performSend(data[`${functionType}Off`]);
-          delete this.waterFunctionTimeouts[functionType];
-
-          // HomeKit aktualisieren
-          const service = this.serviceManager.getService(functionType);
-          if (service) {
-            service.updateCharacteristic(this.Characteristic.Active, this.Characteristic.Active.INACTIVE);
-            service.updateCharacteristic(this.Characteristic.InUse, this.Characteristic.InUse.NOT_IN_USE);
-          }
-        }, 60000);
-      }
-    }
-  }
-
-  destroy() {
-    Object.values(this.waterFunctionTimeouts).forEach((timeout) => {
-      if (timeout) clearTimeout(timeout);
     });
-    this.waterFunctionTimeouts = {};
+
+    // Service 2: Outlet for energy saving mode
+    this.serviceManagerEnergy = new ServiceManagerTypes[serviceManagerType](`${name} Energy`, Service.Outlet, log);
+    this.serviceManagerEnergy.addToggleCharacteristic({
+      name: 'energySavingState',
+      type: Characteristic.On,
+      getMethod: this.getCharacteristicValue,
+      setMethod: this.setCharacteristicValue,
+      bind: this,
+      props: {
+        onData: data.energyOn,
+        offData: data.energyOff
+      }
+    });
+
+    // Service 3: Valve for bidet/shower function 
+    this.serviceManagerBidet = new ServiceManagerTypes[serviceManagerType](`${name} Bidet`, Service.Valve, log);
+    this.serviceManagerBidet.addToggleCharacteristic({
+      name: 'bidetState',
+      type: Characteristic.Active,  // Valve uses Active characteristic for On/Off
+      getMethod: this.getCharacteristicValue,
+      setMethod: this.setCharacteristicValue,
+      bind: this,
+      props: {
+        onData: data.bidetOn,
+        offData: data.bidetOff
+      }
+    });
+    // Optionally, you might set Valve Type characteristic if desired (e.g., bidet vs. shower)
+
+    // Service 4: Fan for dryer
+    this.serviceManagerFan = new ServiceManagerTypes[serviceManagerType](`${name} Dry Fan`, Service.Fan, log);
+    this.serviceManagerFan.addToggleCharacteristic({
+      name: 'fanState',
+      type: Characteristic.On,  // Fan service uses On for power
+      getMethod: this.getCharacteristicValue,
+      setMethod: this.setCharacteristicValue,
+      bind: this,
+      props: {
+        onData: data.fanOn,
+        offData: data.fanOff
+      }
+    });
+
+    // Designate one of these as the primary service for the accessory (the first service is typically primary).
+    // We will treat the main power Switch as primary:
+    this.serviceManager = this.serviceManagerPrimary;
+  }
+
+  // Override getServices to include all our services
+  getServices() {
+    // Start with the standard Accessory Information service:
+    const services = this.getInformationServices();
+    // Add primary service (already set as this.serviceManager in setup)
+    services.push(this.serviceManager.service);
+    // Add additional services:
+    services.push(this.serviceManagerEnergy.service);
+    services.push(this.serviceManagerBidet.service);
+    services.push(this.serviceManagerFan.service);
+    return services;
   }
 }
 
